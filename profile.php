1c8a7b52be6f11f73a54e6080f7734e857d237e8
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-01-14 13:54:24 -0700

    Added bootstrap to make the navigation bar the same as the home page which changed a little bit extra but still looks okay still needs work

--- profile.php
+++ profile.php
@@ -4,8 +4,51 @@
     <title>GamePlan Profile</title>
     <link href="Styles_Index.css" type="text/css" rel="stylesheet">
     <link rel="icon" type="image/png" href="GamePlan_logo_2.png">
-    <script type="text/javascript" src="scripts.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="scripts.js"></script>
+    <style>
+        #Goalsheader{
+            text-align: center;
+        }
+        #signlogin {
+            align-content: right;
+            align-items: right;
+            text-align: right;
+        }
+        #panels {
+            margin-top: 325px;
+            background-color: #EAEAEA;
+            font-family: Georgia, "Times New Roman", Times, serif;
+        }
+        #panels > h3 {
+            font-size: 75px;
+        }
+        #panels > p {
+            font-size: 16px;
+        }
+        #Abouthead {
+            text-align: center;
+        }
+        img {
+            display: none
+        }
+        h3 {
+            font-family: Elephant;
+            text-align: center;
+        }
+        #Goalsheader > p {
+            width: 500px;
+            align-content: center;
+            text-align: center;
+            margin-left: 450px;
+        }
+    </style>
 </head>
+
 <body>
 <div id="Main">
     <div id="TopSpace">
@@ -30,11 +73,17 @@
         </div>
         <div id="Navigation">
             <!-- everything navigation goes here (other than signing up and logging out and creating new accounts) -->
-            <div id="control_nav">
-                <a href="index.php"><div class="NavigationBox" style="float: left; margin-right: 3px; margin-left: 3px;"><h3 style="margin-top: 2px; text-align: center;">Home</h3></div></a>
-                <a href="profile.php"><div class="NavigationBox" style="float: left; margin-right: 3px; margin-left: 3px;"><h3 style="margin-top: 2px; text-align: center;">Profile</h3></div></a>
-                <a href="Ticket_Page.php"><div class="NavigationBox" style="float: left; margin-right: 3px; margin-left: 3px;"><h3 style="margin-top: 2px; text-align: center;">Buy Tickets</h3></div></a>
-            </div>
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <ul class="nav nav-tabs">
+                        <li role="presentation" class="active"><a href="index.php">Home</a></li>
+                        <li role="presentation"><a href="profile.php">Profile</a></li>
+                        <li role="presentation"><a href="Ticket_Page.php">Events</a></li>
+                        <div id="signlogin">
+                            <button type="button" class="btn btn-default navbar-btn"><a href="loginPage.php"> Sign in</a></button>
+                            <button type="button" class="btn btn-default navbar-btn"><a href="createAccount.php"> Create Account</a></button>
+                        </div>
+                    </ul>
         </div>
     </div>
     <div id="Name"></div>
