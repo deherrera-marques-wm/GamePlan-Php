baa75406c6122c3d2462775a063a8d3ec529f9db
Start Coding Student 1 <session1@START-Coding-MBP-07.local>
2016-01-15 13:32:35 -0700

    fixed (kind of) the image slider moving the rest of the page. The rest is up to you Nick!

--- index.php
+++ index.php
@@ -63,44 +63,45 @@
 
     </div>
 </nav>
+<div id="Test" style="width: 100%; height: 706px; background-color: #fefefe">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="GamePlan%20Pictures/GamePlan%201.jpg" style="align-content: center" alt="Chania">
+            </div>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <img src="GamePlan%20Pictures/GamePlan%201.jpg" style="align-content: center" alt="Chania">
-        </div>
+            <div class="item">
+                <img src="GamePlan%20Pictures/GamePlan%202.png" alt="Chania">
+            </div
 
-        <div class="item">
-            <img src="GamePlan%20Pictures/GamePlan%202.png" alt="Chania">
-        </div
+            <div class="item">
+                <img src="GamePlan%20Pictures/GamePlan%203.jpg" alt="Flower">
+            </div>
 
-        <div class="item">
-            <img src="GamePlan%20Pictures/GamePlan%203.jpg" alt="Flower">
-        </div>
+            <div class="item">
+                <img src="GamePlan%20Pictures/GamePlan%204.jpg" alt="Flower">
+            </div>
+            </div>
 
-        <div class="item">
-            <img src="GamePlan%20Pictures/GamePlan%204.jpg" alt="Flower">
-        </div>
-        </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </div>
 <div id="panels">
     <div id="Abouthead">
